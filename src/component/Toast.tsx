5d4563241a6a5e5a0479e00ac18a146f8c2ee866
ldslds449 <ldslds449@gmail.com>
2025-05-10 21:47:24 +0800

    💄 Center the toast

--- src/component/Toast.tsx
+++ src/component/Toast.tsx
@@ -1,18 +1,17 @@
 import styled from "styled-components";
 
 const StyledToast = styled.div<{ $hidden?: boolean; }>`
-    visibility: ${props => props.$hidden ? "hidden" : "visible"}; /* Hidden by default. Visible on click */
-    min-width: 250px; /* Set a default minimum width */
-    margin-left: -125px; /* Divide value of min-width by 2 */
-    background-color: #333; /* Black background color */
-    color: #fff; /* White text color */
-    text-align: center; /* Centered text */
-    border-radius: 2px; /* Rounded borders */
-    padding: 16px; /* Padding */
-    position: fixed; /* Sit on top of the screen */
-    z-index: 1; /* Add a z-index if needed */
-    left: 50%; /* Center the snackbar */
-    bottom: 30px; /* 30px from the bottom */
+    visibility: ${props => props.$hidden ? "hidden" : "visible"};
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    border-radius: 2px;
+    padding: 16px;
+    position: fixed;
+    z-index: 1;
+    left: 50%;
+    bottom: 30px;
+    transform: translate(-50%, -50%);
 `;
 
 type ToastProps = {
